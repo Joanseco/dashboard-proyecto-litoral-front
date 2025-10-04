e642a382f4424c9decafc54d70a07dcd011fcc91
Joanseco <joanseco5@gmail.com>
2025-10-04 15:24:41 -0500

    Cambios para la url de render

--- src/components/sections/UsersSection.jsx
+++ src/components/sections/UsersSection.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://dashboard-proyecto-litoral-backend.onrender.com/api';
 
 const UsersSection = () => {
     // Estados principales
